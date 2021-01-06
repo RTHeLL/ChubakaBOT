e6645c3ef11216c300134013da35d95007c391a4
RTHeLL <fak65ggg@ya.ru>
2021-01-07 00:29:14 +0300

    07012021_0029

--- classes/mysql.py
+++ classes/mysql.py
@@ -1,3 +1,4 @@
+import logging
 import pymysql
 from pymysql import MySQLError
 import configparser
@@ -7,14 +8,23 @@ config.read("data/mysql_config.ini")
 
 
 class MySQL:
-    # MySQL
+    # Connection
     try:
         connection = pymysql.connect(config["MySQL_DATA"]["SQL_HOST"], config["MySQL_DATA"]["SQL_USER"],
                                      config["MySQL_DATA"]["SQL_PASS"], config["MySQL_DATA"]["SQL_DB"])
-        print("MySQL success connected!")
+        logging.log(logging.INFO, "MySQL success connected!")
     except MySQLError as e:
-        print("Error code:", e)
-        print("Error message:", e.args[0])
+        logging.log(logging.FATAL, e)
+
+    def check_user(self, vk_id):
+        with self.connection.cursor() as cursor:
+            sql = "SELECT `ID` FROM `users` WHERE `VK_ID`=%s"
+            cursor.execute(sql, vk_id)
+            result = cursor.fetchone()
+            if result == "None":
+                return False
+            else:
+                return result
 
     def get_user(self, vk_id):
         with self.connection.cursor() as cursor:
